65c51366b8552bbcf61664dd568ae667e7683a57
dickson-ank <dickson.ankamah8@gmail.com>
2025-09-17 17:35:37 +0000

    Configure GitHub Pages deployment

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+	return (
+		<html>
+			<body>
+				<h1>404 - Page Not Found</h1>
+				<p>The page you are looking for does not exist.</p>
+				<a href="/tier3topology_aws/">Go back home</a>
+			</body>
+		</html>
+	)
+} 
