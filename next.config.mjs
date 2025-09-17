48da9f1779103babaf1fef45aea875f608283c82
dickson-ank <dickson.ankamah8@gmail.com>
2025-09-17 06:57:10 +0000

    Modify config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
 const isProd = process.env.NODE_ENV === "production";
 const repoName = "tier3topology_aws";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
+  assetPrefix: isProd ? `/${repoName}` : "",
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
