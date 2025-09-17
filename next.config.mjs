9d591a46575c528137b7df82b3392c22d12c8dc0
dickson-ank <dickson.ankamah8@gmail.com>
2025-09-17 06:45:10 +0000

    Fix bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,13 @@
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-
 const repoName = "tier3topology_aws";
 
 const nextConfig = {
-  output: "export", // Required for static export
+  output: "export",
   basePath: isProd ? `/${repoName}` : "",
   assetPrefix: isProd ? `/${repoName}/` : "",
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
