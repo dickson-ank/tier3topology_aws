94b2835462cadf63b3b0da3f0d8596a91ad84ac5
dickson-ank <dickson.ankamah8@gmail.com>
2025-09-17 14:35:18 +0000

    Setup Github pages deployment

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/tier3topology_aws',
-  assetPrefix: '/tier3topology_aws/', 
-}
-
-export default nextConfig;
