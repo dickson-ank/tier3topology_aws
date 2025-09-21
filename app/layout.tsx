bb7ba81502ae52511a46c7c4eb97a4ef011a5ff9
dickson-ank <dickson.ankamah8@gmail.com>
2025-09-21 10:36:44 +0000

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,14 @@ export const metadata: Metadata = {
   title: "AWS 3-Tier Architecture Project",
   description: "A step-by-step guide to building a 3-tier architecture on AWS",
   generator: "Dickson Ankamah",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+  },
+  manifest: "/site.webmanifest",
 }
 
 export default function RootLayout({
