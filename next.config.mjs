ee78b50867d585e42d2e9ef43d62ca2b38ccffc9
dickson-ank <dickson.ankamah8@gmail.com>
2025-09-21 00:48:50 +0000

    Edit workflow to fix image display error

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   output: "export",
   basePath: "/tier3topology_aws",
   assetPrefix: "/tier3topology_aws/",
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
