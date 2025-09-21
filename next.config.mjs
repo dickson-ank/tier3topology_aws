1b59c90d67c8a3c63f52f8abb8dfb0b449484331
dickson-ank <dickson.ankamah8@gmail.com>
2025-09-21 01:17:36 +0000

    Edit path settings in config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/tier3topology_aws",
-  assetPrefix: "/tier3topology_aws/",
+  output: 'export',
+  basePath: '/tier3topology_aws',
+  assetPrefix: '/tier3topology_aws/',
   images: {
-    unoptimized: true,
+    loader: 'custom',
+    loaderFile: './my_loader.ts',
   },
   eslint: {
     ignoreDuringBuilds: true,
