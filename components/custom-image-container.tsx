a2c4d675fdc80cf54e21761e47e3372ba9d12243
dickson-ank <dickson.ankamah8@gmail.com>
2025-10-06 23:57:00 +0000

    Modify image tag

--- components/custom-image-container.tsx
+++ components/custom-image-container.tsx
@@ -4,19 +4,21 @@ import { ReactNode } from "react"
 interface ImageAttributes{
     src: string
     alt: string
+    id?: string
     className?: string
     children?: ReactNode
 
     selectedImage: (src: string) => void
 }
 
-export function ImageContainer({src, alt, className, children, selectedImage} : ImageAttributes){
+export function ImageContainer({src, alt, id,className, children, selectedImage} : ImageAttributes){
     return(
 
         <div className={`bg-card my-6 border border-border rounded-lg p-1 ${className}`}>
             <img
             src={src}
             alt={alt}
+            id={id}
             className="w-full h-auto rounded cursor-pointer hover:opacity-100 transition-opacity"
             onClick={() => selectedImage(src)}
             />
--- components/pagesections/step-1.tsx
+++ components/pagesections/step-1.tsx
@@ -14,20 +14,19 @@ export function Step1({setSelectedImage}: Step1Props){
                This is a crucial step in setting up a secure and scalable architecture. <br/>
                 <br />
                 • Search for VPC in the AWS Management Console and open the VPC Dashboard. <br/>
-                • Click on "Create VPC". <br/>
+                • Click on <span className="text-primary font-semibold">Create VPC</span>. <br/>
                 • Choose "VPC only" and provide a name ("webapp-network" in my case). <br/>
-                • Set the IPv4 CIDR block to "10.0.0.0/16". <br/>
-                • Review and Click "Create VPC". <br/>
+                • Set the IPv4 CIDR block to <span className="font-mono text-primary">10.0.0.0/16</span><br/>
+                • Review and Click <span className="text-black text-sm font-semibold px-2 py-0 bg-aws rounded-2xl">Create VPC</span><br/>
             </p>
             <ImageContainer className="mb-1"src="./vpc-create.png" alt="Container setup" selectedImage={setSelectedImage} />
             <ImageContainer className="mt-1"src="./vpc-create2.png" alt="Container setup" selectedImage={setSelectedImage} />
-            <hr />
             <p className="text-muted-foreground mb-6 text-pretty text-sm sm:text-base">
               Next we will create our subnets. One each for presentation Tier and Application Tier, <br />
               and Two for the data tier for extra security and high availability. <br />
               as shown in the image below. <br />
             </p>
-            <ImageContainer className="mt-1"src="./draw.io.vpc-subnet-base.png" alt="Container setup" selectedImage={setSelectedImage} />
+            <ImageContainer id="vpc-subnet-base" className="mt-1"src="./draw.io.vpc-subnet-base.png" alt="Container setup" selectedImage={setSelectedImage} />
             <p className="text-muted-foreground mb-6 text-pretty text-sm sm:text-base">
               Expand the VPC Dashboard menu if closed and select Subnets
             </p>
@@ -39,13 +38,20 @@ export function Step1({setSelectedImage}: Step1Props){
                 cloudformation template the vpc name will prepend the subnet names.</span>
               to avoid confusion as you follow along, we will omit that here
               • Name it "public-subnet" since it's the only public subnet <br />
-              • Set the Availability Zone to your preference (e.g., us-east-1a) <br />
-                note it somewhere <br />
+              • Set the Availability Zone to your preference (e.g., us-east-1a) and note it somewhere<br />
               • Set the IPv4 CIDR block to <span className="font-mono text-primary">10.0.1.0/24</span> <br />
               • Review and click <span className="text-black text-sm font-semibold px-2 py-0 bg-aws rounded-2xl">Create Subnet</span> <br />
             </Paragraph>
             <ImageContainer className="mb-1"src="./create-subnet1.jpeg" alt="Subnet create screenshot" selectedImage={setSelectedImage} />
             <ImageContainer className="mt-1"src="./create-subnet2.jpeg" alt="Subnet create screenshot" selectedImage={setSelectedImage} />
+
+            <Paragraph>
+              Repeat the process to create the three private subnets <br />
+              • Name them "private-subnet-1", "private-subnet-2", and "private-subnet-3" respectively <br />
+              • Put the first 2 private subnets in same Availability Zone as the public subnet <br />
+                and put the last subnet in a different Availability Zone <br />
+                refer the image <a href="vpc-subnet-base">here</a> and set their CIDRs as shown <br />
+            </Paragraph>
             <div className="space-y-6">
               <div className="gradient-card p-4 sm:p-6 rounded-lg border border-border">
                 <h3 className="font-semibold text-foreground mb-4 text-sm sm:text-base">Creating Your First Lambda</h3>
@@ -59,9 +65,6 @@ export function Step1({setSelectedImage}: Step1Props){
                   <div>{"}"}</div>
                 </div>
               </div>
-
-              <ImageContainer src="./placeholder.svg" alt="Lambda function creation" selectedImage={setSelectedImage} />
-
             </div>
           </ProjectSection>
     )
