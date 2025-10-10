c605b6ec996169a1d35aa81c36ac03cdd2958407
dickson-ank <dickson.ankamah8@gmail.com>
2025-10-10 21:51:24 +0000

    Add sideways scroll function

--- components/pagesections/step-6.tsx
+++ components/pagesections/step-6.tsx
@@ -12,16 +12,61 @@ interface Step6Props {
 export function Step6({setSelectedImage}: Step6Props){
     return(
         <ProjectSection id="step-6" title="Step 6: Testing" onImageClick={setSelectedImage}>
+            <Paragraph>
+              In this final step, we will test the entire setup to ensure that all components are functioning correctly.
+               We will verify that the App server can communicate with the database as well 
+               with the Web Sever.
+               We will also test if the Web server is serving the web application properly. <br />
+               We will test if only permitted access is allowed to all the Instances. <br /> <br />
+               We will need a terminal for this step. I will be using WSL terminal on Windows.
+
+               We 
+            </Paragraph>
+
+
+            <Paragraph>
+              <span className="block mb-2">
+               First download this html file and save it to your home directory(or the directory you will be using the terminal in).
+                This file will be used to test if the web server is serving the web application properly. <br />
+                <a
+                  href="./webapp-index.html"
+                  download
+                  className="px-1 py-1 bg-primary text-primary-foreground rounded-lg font-small font-mono 
+                  hover:bg-primary/90 transition-colors text-xs sm:text-xs md:text-sm lg:text-sm"
+                >
+                  📄Download webapp-index.html
+                </a>
+              </span>
+              And then upload they key pair pem file you created in Step 4 to the same directory.
+            </Paragraph>
+            <ImageContainer src="./terminal-show-current-files.png" 
+            alt="Uploading the key pair and html file to the terminal directory" selectedImage={setSelectedImage}/>
+
+            <Paragraph>
+              Rename the file to index.html <br />
+              The file kept interfering with other index.html files while developing the project 
+              that's the reason I named it webapp-index.html but you must rename it to index.html <br />
+              Run this command: <br />
+              <span className="bg-muted p-3 sm:p-4 rounded font-mono text-xs sm:text-sm block w-fit">
+                <span className="text-muted-foreground mb-1 block">
+                  scp -i YOUR-KEY-PAIR.pem webapp-index.html ec2-user@YOUR-WEB-SERVER-IP:~/
+                </span>
+                <div></div>
+              </span>
+            </Paragraph>
+            <ImageContainer src="./rename-to-index.png" 
+            alt="Renaming the html file to index.html" selectedImage={setSelectedImage}/>
+
             <div className="space-y-6 mt-6">
               <div className="gradient-card p-4 sm:p-6 rounded-lg border border-border">
-                      <h3 className="font-semibold text-foreground mb-4 text-sm sm:text-base">Testing Your Deployment</h3>
                       <p className="text-xs sm:text-sm text-muted-foreground text-pretty mb-4">
-                        Use these commands to test your deployed serverless architecture and verify all components are working
-                        correctly.
+                       Run the following command to upload the html file to the web server instance. <br />
                       </p>
                       <div className="bg-muted p-3 sm:p-4 rounded font-mono text-xs sm:text-sm">
-                        <div className="text-muted-foreground mb-1"># Test API endpoint</div>
-                        <div>curl -X GET https://your-api-id.execute-api.region.amazonaws.com/prod/hello</div>
+                        <div className="text-muted-foreground mb-1">
+                          scp -i YOUR-KEY-PAIR.pem webapp-index.html ec2-user@YOUR-WEB-SERVER-IP:~/
+                        </div>
+                        <div></div>
                       </div>
                 </div>
             </div>
@@ -52,16 +97,6 @@ export function Step6({setSelectedImage}: Step6Props){
                         sudo systemctl enable --now mariadb
                     </div>
                 </div>
-                        <div>
-                          <a
-                            href="./webapp-index.html"
-                            download
-                            className="px-1 py-1 bg-primary text-primary-foreground rounded-lg font-small font-mono 
-                            hover:bg-primary/90 transition-colors text-xs sm:text-xs md:text-sm lg:text-sm"
-                          >
-                            📄Download webapp-index.html
-                          </a>
-                        </div>
  
         </ProjectSection>
     )
